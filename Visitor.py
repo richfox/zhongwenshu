2034e0f7cbe9c1a4c70c8490bdb697cc74607e3e
Xiang Fu <XFu@intl.isdgroup.com>
2017-11-02 17:17:54 +0100

    中文元素提取

--- Visitor.py
+++ Visitor.py
@@ -6,18 +6,30 @@
 
 import sys
 import lxml.html
-
+import openpyxl.workbook
+import re
 
 
 class Visitor:
     def __init__(self,file):
-        htmltree = lxml.html.parse(file)
-        print(lxml.html.tostring(htmltree,pretty_print=True))
+        htmlstring = open(file,'r').read()
+        htmltree = lxml.html.document_fromstring(htmlstring.decode('gb2312'))
         self._htmltree = htmltree
 
     def searchOrderGoods(self):
-        res = self._htmltree.xpath('//*[@id="normalorder"]//table//*[@name="productname"]')
-        print(lxml.html.tostring(res[0],pretty_print=True))
+        basepath = '//*[@id="normalorder"]//table//*[@name="productname"]'
+        books = self._htmltree.xpath(basepath)
+        titles = self._htmltree.xpath(basepath+'/@title')
+
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        for i,title in enumerate(titles):
+            #lxml.html.tostring(book,pretty_print=True,encoding='utf-8')
+            col = 'A'
+            row = str(i+1)
+            cell = col + row
+            ws[cell] = title
+        wb.save('_books.xlsx')
         
 
 
