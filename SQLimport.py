1e6bdffd4071fa875bc783e4b67bfb919f0d7b4a
Xiang Fu <XFu@intl.isdgroup.com>
2018-02-16 11:49:58 +0100

    python mysql

---
+++ SQLimport.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8-*-
+#＝＝＝＝＝＝数据库输入系统＝＝＝＝＝＝＝＝
+#Python 2.7
+#Author: Xiang Fu
+#Email: tech@zhongwenshu.de
+
+
+import pymysql.cursors
+
+
+
+def SQLimport():
+    print("Import start...\n")
+    print("Finished.")
