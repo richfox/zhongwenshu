b7495b3ed1a1da003b410c40ea282216649f6c74
Xiang Fu <XFu@intl.isdgroup.com>
2018-02-22 15:42:51 +0100

    spider to mysql

---
+++ SpiderToSQL.py
@@ -0,0 +1,26 @@
+#-*-coding:utf-8-*-
+#＝＝＝＝＝＝登书系统＝＝＝＝＝＝＝＝
+#Python 2.7
+#Author: Xiang Fu
+#Email: tech@zhongwenshu.de
+
+
+import pymysql
+import requests
+import lxml.etree
+import re
+
+
+
+def SpiderToSQL(sqls):
+    print("Spider to SQL start...\n")
+
+    for host,(username,password,dbname,charset,urls) in sqls.items():
+        connection = pymysql.connect(host=host,user=username,password=password,db=dbname,charset=charset)
+        try:
+            for url,tag in urls.items():
+                html = requests.get(url).text
+        finally:
+            connection.close()
+
+    print("Finished.")
