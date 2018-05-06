7836529b634f8ab20d59f2fd197eda2bfd6ee43a
xiang fu <fuxiang1978@gmail.com>
2018-05-06 18:39:39 +0200

    verbesserung

--- SpiderToSQL.py
+++ SpiderToSQL.py
@@ -136,21 +136,21 @@ def SpiderToSQL(sqls):
 
                 with connection.cursor() as cursor:
                     sql = "INSERT INTO " + goodstable + " (`goods_id`, `cat_id`, `goods_sn`,`goods_name`,\
-                    `goods_name_style`, `click_count`, `brand_id`, `provider_name`, `goods_number`,\
-                    `goods_weight`, `market_price`, `virtual_sales`, `shop_price`, `promote_price`,\
-                    `promote_start_date`, `promote_end_date`, `warn_number`, `keywords`, `goods_brief`,\
-                    `goods_desc`, `goods_thumb`, `goods_img`, `original_img`, `is_real`, `extension_code`,\
-                    `is_on_sale`, `is_alone_sale`, `is_shipping`, `integral`, `add_time`, `sort_order`,\
-                    `is_delete`, `is_best`, `is_new`, `is_hot`, `is_promote`, `bonus_type_id`, `last_update`,\
-                    `goods_type`, `seller_note`, `give_integral`, `rank_integral`, `suppliers_id`, `is_check`) \
-                    VALUES (NULL, %s, %s, %s,\
-                    '+', '0', '0', '', '0',\
-                    '0', '0', '', '0', '0.00',\
-                    '0', '0', '1', '', '',\
-                    '', '', '', '', '1', '',\
-                    '0', '1', '0', '0', %s, '100',\
-                    '0', '0', '1', '0', '0', '0', '0',\
-                    %s, '', '-1', '-1', '0', NULL)"
+                        `goods_name_style`, `click_count`, `brand_id`, `provider_name`, `goods_number`,\
+                        `goods_weight`, `market_price`, `virtual_sales`, `shop_price`, `promote_price`,\
+                        `promote_start_date`, `promote_end_date`, `warn_number`, `keywords`, `goods_brief`,\
+                        `goods_desc`, `goods_thumb`, `goods_img`, `original_img`, `is_real`, `extension_code`,\
+                        `is_on_sale`, `is_alone_sale`, `is_shipping`, `integral`, `add_time`, `sort_order`,\
+                        `is_delete`, `is_best`, `is_new`, `is_hot`, `is_promote`, `bonus_type_id`, `last_update`,\
+                        `goods_type`, `seller_note`, `give_integral`, `rank_integral`, `suppliers_id`, `is_check`) \
+                        VALUES (NULL, %s, %s, %s,\
+                        '+', '0', '0', '', '0',\
+                        '0', '0', '', '0', '0.00',\
+                        '0', '0', '1', '', '',\
+                        '', '', '', '', '1', '',\
+                        '0', '1', '0', '0', %s, '100',\
+                        '0', '0', '1', '0', '0', '0', '0',\
+                        %s, '', '-1', '-1', '0', NULL)"
                     cursor.execute(sql,(catid,sn,title,addtime,gtype))
 
                     #唯一商品编号
