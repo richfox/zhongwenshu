b85457b1a925f6d9e18478ff9cb35ef13065d379
Xiang Fu <xfu@Dortmund.de>
2020-11-06 11:49:16 +0100

    comment

--- logis.py
+++ logis.py
@@ -354,12 +354,14 @@ def generate_logis_expression_from_sql(server,logis):
             for sn in sns:
                 orderinfos = {}
                 if regex == "1":
+                    #注意字段best_time被用于记录微信号
                     sql = "SELECT `order_id`,`order_sn`,`best_time`,`money_paid` FROM " + orderInfoTable + " WHERE `order_sn` REGEXP %s AND `order_id` \
                         in (SELECT `order_id` FROM " + orderGoodsTable + " WHERE `goods_id` = %s);"
                     cursor.execute(sql,(sn,template))
                     logging.info(sql % ("'"+sn+"'","'"+template+"'"))
                     orderinfos = cursor.fetchall()
                 elif regex == "0":
+                    #注意字段best_time被用于记录微信号
                     sql = "SELECT `order_id`,`order_sn`,`best_time`,`money_paid` FROM " + orderInfoTable + " WHERE `order_sn` = %s AND `order_id` \
                         in (SELECT `order_id` FROM " + orderGoodsTable + " WHERE `goods_id` = %s);"
                     cursor.execute(sql,(sn,template))
