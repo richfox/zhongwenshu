41bf5a08c9a2a3f631b574fc683d34776f043b13
xiang fu <fuxiang1978@gmail.com>
2020-12-19 16:39:20 +0100

    correction

--- fetchModule.bat
+++ fetchModule.bat
@@ -4,13 +4,14 @@ rem author: xfu
 rem email: tech@zhongwenshu.de
 rem fetch module for python to run dangdang.py
 
-pip isntall openpyxl
-pip isntall requests
-pip isntall lxml
-pip isntall Pillow
+pip install openpyxl
+pip install requests
+pip install lxml
+pip install Pillow
 pip install pymysql
 pip install pinyin
 pip install jsonpath
+pip install pywin32
 
 echo finished
 pause
