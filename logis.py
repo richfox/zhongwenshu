a711b1a64e9788a10e229268e5b3b24b47ebe189
richfox <fuxiang1978@gmail.com>
2021-01-30 15:14:19 +0100

    test

--- logis.py
+++ logis.py
@@ -353,7 +353,7 @@ def get_customer_forecast(htmltree):
                             (company,notes) = list(dicitems)[len(dicitems)-1]
                             notes.append(label.strip())
                             continue
-                forecast.append(split_logis_expr_and_token(node))
+                    forecast.append(split_logis_expr_and_token(node))
     return forecast
 
 
@@ -543,9 +543,9 @@ class TestLogis(unittest.TestCase):
             <div class="section" id="railway">
             <div class="title"><span>铁路</span></div>
             <div class="descrip">
-            <p><span style="color:#330099">邮政23232445<span class="da">到仓</span> ,+ JT798797947646<span class="da">到仓</span></span></p>
+            <p><span style="color:#330099">邮政23232445<span class="da">到仓</span> ,+ JT798797947646<span class="fa">德仓发出</span></span></p>
             <p><span style="color:#330099">中通232324452-1-2<span class="da">到仓</span> ,+ JT7987979476461</span></p>
-            <p><span style="color:#330099">中通快递232324452-2-2:todo ,，+ JT7987979476462:16kg：文具<span class="da">到仓</span></span></p>
+            <p><span style="color:#330099">中通快递232324452-2-2:玩具 ,，+ JT7987979476462:16kg：文具<span class="da">到仓</span>  </span></p>
             <p>各种书</p>
             <p>&nbsp;</p>
             </div>
@@ -567,7 +567,18 @@ class TestLogis(unittest.TestCase):
         self.assertEqual(list(res.keys())[2],"232324452-1-2")
         company,notes = list(res.values())[3]
         self.assertEqual(has_special_label(notes),False)
-        self.assertEqual(list(res.values())[4],(get_logis_companies()["中通"],["todo"]))
+        self.assertEqual(list(res.values())[4],(get_logis_companies()["中通"],["玩具"]))
         company,notes = list(res.values())[5]
         self.assertEqual(has_special_label(notes),True)
         self.assertEqual(len(notes),3)
+
+        manifest = []
+        for forecast in get_customer_forecast(htmltree):
+            for cnsn,(company,notes) in forecast.items():
+                if not has_special_label(notes):
+                    manifest.append((cnsn,company,notes))
+        expr = generate_manifest_expression(manifest)
+        print(expr)
+        self.assertEqual(len(manifest),2)
+        self.assertEqual(manifest[0][0],"JT7987979476461")
+        self.assertEqual(manifest[1][1],get_logis_companies()["中通"])
