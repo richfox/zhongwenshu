e43d7919d4728f959163951c075ca54f72bd1269
richfox <fuxiang1978@gmail.com>
2023-03-14 22:28:52 +0100

    `discount7`, `discount19`, `goods_amount7`, `goods_amount19` 字段已不复存在

--- ExcelToSQL.py
+++ ExcelToSQL.py
@@ -331,8 +331,7 @@ def ExcelToSQLGBuy(sqls,params):
                         `shipping_fee`, `insure_fee`, `pay_fee`, `pack_fee`, `card_fee`, `money_paid`, \
                         `surplus`, `integral`, `integral_money`, `bonus`, `order_amount`, `from_ad`, `referer`, \
                         `add_time`, `confirm_time`, `pay_time`, `shipping_time`, `pack_id`, `card_id`, `bonus_id`, `invoice_no`, \
-                        `extension_code`, `extension_id`, `to_buyer`, `pay_note`, `agency_id`, `inv_type`, `tax`, `is_separate`, \
-                        `parent_id`, `discount`, `discount7`, `discount19`, `goods_amount7`, `goods_amount19`) \
+                        `extension_code`, `extension_id`, `to_buyer`, `pay_note`, `agency_id`, `inv_type`, `tax`, `is_separate`, `parent_id`, `discount`) \
                         VALUES (NULL, %s, '0', '1', '3', '2', \
                         %s, %s, '0', '0', '0', %s, %s, %s, '', %s, \
                         %s, %s, %s, %s, %s, %s, %s, \
@@ -340,8 +339,7 @@ def ExcelToSQLGBuy(sqls,params):
                         '0.00', '0.00', '0.00', '0.00', '0.00', %s, \
                         '0.00', '0', '0.00', '0.00', %s, '0', '表单大师', \
                         %s, %s, %s, '0', '0', '0', '0', '', \
-                        '', '0', '', %s, '0', '', '0.00', '0', \
-                        '0', '0.00', '0.00', '0.00', %s, '0.00')"
+                        '', '0', '', %s, '0', '', '0.00', '0', '0', '0.00')"
                     cursor.execute(sql,(ordernr,
                                     infos[orderheads[u'name']],str(country),infos[orderheads[u'address']],infos[orderheads[u'postcode']],telefon,email,
                                     wechat,infos[orderheads[u'postcode']],ordercomment,shipping,shippingname,payid,payname,
