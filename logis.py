ec623398c1be72fc0a6b0bdd81bfd5af18810067
richfox <fuxiang1978@gmail.com>
2025-07-26 10:22:36 +0200

    #120

--- logis.py
+++ logis.py
@@ -20,89 +20,7 @@ import Visitor
 import unittest
 
 
-def import_logis_to_sql(server,logis):
-    print("Starting import logis info to database...\n")
 
-    mysql = server["mysql"]
-    connection = pymysql.connect(host=mysql[0],user=mysql[1],password=mysql[2],database=mysql[3],charset=mysql[4])
-
-    #区分测试和主数据库
-    rinterTable = ""
-    logiscnTable = ""
-    logisdeTable = ""
-    if mysql[3] == 'zhongw_test':
-        rinterTable = "zws_test_railway_inter"
-        logiscnTable = "zws_test_logis_cn"
-        logisdeTable = "zws_test_logis_de"
-    elif mysql[3] == 'zhongwenshu_db1':
-        rinterTable = "zws_railway_inter"
-        logiscnTable = "zws_logis_cn"
-        logisdeTable = "zws_logis_de"
-
-    try:
-        railway_sn = ""
-        inter_company = ""
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-        cn_packet = {}
-        de_packet = {}
-
-        for area,info in logis.items():
-            if area == "inter":
-                for k,v in info.items():
-                    inter_company = k
-                    railway_sn = v
-            elif area == "cn":
-                for k,v in info.items():
-                    for sn in v:
-                        cn_packet[sn] = k
-            elif area == "de":
-                for k,v in info.items():
-                    for sn in v:
-                        de_packet[sn] = k
-
-
-        #先写入国际段物流信息，因为国际段主键是作为其他表的外键
-        #再写入国内段和德国段物流信息，可以不分先后
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO " + rinterTable + " (`id`, `railway_sn`, `inter_log`, `inter_time`, `inter_status`, `inter_company`) \
-                  VALUES (NULL, %s, '', %s, '-1', %s);"
-            cursor.execute(sql,(railway_sn,timestamp,inter_company))
-
-            #log
-            LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-            logging.basicConfig(filename='mylogis.log',level=logging.DEBUG,format=LOG_FORMAT)
-            logging.info("----------------------------------------------------------------") #分割线
-            logging.info(sql % ("'"+railway_sn+"'","'"+timestamp+"'","'"+inter_company+"'"))
-
-            sql = "SELECT `id` FROM " + rinterTable + " WHERE `railway_sn`=%s"
-            cursor.execute(sql,railway_sn)
-            rinterid = cursor.fetchone()[0]
-            print("inter id : " + str(rinterid))
-
-            for sn,company in cn_packet.items():
-                sql = "INSERT INTO " + logiscnTable + " (`id`, `cn_packet_sn`, `railway_id`, `cn_log`, `cn_time`, `cn_status`, `cn_company`) \
-                    VALUES (NULL, %s, %s, '', %s, '-1', %s);"
-                if not company:
-                    mainsn = ['']
-                    subsns = [[]]
-                    companycode =['']
-                    notes = [[]]
-                    split_token(sn,mainsn,subsns,companycode,notes)
-                    company = companycode[0]
-                cursor.execute(sql,(sn,rinterid,timestamp,company))
-                logging.info(sql % ("'"+sn+"'",rinterid,"'"+timestamp+"'","'"+company+"'"))
-
-            for sn,company in de_packet.items():
-                sql = "INSERT INTO " + logisdeTable + " (`id`, `de_packet_sn`, `railway_id`, `de_log`, `de_time`, `de_status`, `de_company`) \
-                    VALUES (NULL, %s, %s, '', %s, '-1', %s);"
-                cursor.execute(sql,(sn,rinterid,timestamp,company))
-                logging.info(sql % ("'"+sn+"'",rinterid,"'"+timestamp+"'","'"+company+"'"))
-
-        connection.commit()
-    finally:
-        connection.close()
-
-    print("Finished.")
 
 
 def get_logis_labels():
@@ -171,6 +89,9 @@ logisKeywordHeader = get_logis_operators()["keywordheader"] #关键字
 logisSeparator = get_logis_operators()["separator"] #分隔符
 logisExplanator = get_logis_operators()["explanator"] #半角或全角冒号代表说明符
 logisConnector = get_logis_operators()["connector"] #母子单号连接符
+
+
+
 
 #
 #以下若干函数物流表达式解析，不支持括号，和lib_logis.php一致
@@ -300,6 +221,98 @@ def build_subsn(subsns):
 #
 #以上若干函数物和lib_logis.php一致
 #
+
+
+
+#物流单号写入数据库
+def import_logis_to_sql(server,logis):
+    print("Starting import logis info to database...\n")
+
+    mysql = server["mysql"]
+    connection = pymysql.connect(host=mysql[0],user=mysql[1],password=mysql[2],database=mysql[3],charset=mysql[4])
+
+    #区分测试和主数据库
+    rinterTable = ""
+    logiscnTable = ""
+    logisdeTable = ""
+    if mysql[3] == 'zhongw_test':
+        rinterTable = "zws_test_railway_inter"
+        logiscnTable = "zws_test_logis_cn"
+        logisdeTable = "zws_test_logis_de"
+    elif mysql[3] == 'zhongwenshu_db1':
+        rinterTable = "zws_railway_inter"
+        logiscnTable = "zws_logis_cn"
+        logisdeTable = "zws_logis_de"
+
+    try:
+        railway_sn = ""
+        inter_company = ""
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+        cn_packet = {}
+        de_packet = {}
+
+        for area,info in logis.items():
+            if area == "inter":
+                for k,v in info.items():
+                    inter_company = k
+                    railway_sn = v
+            elif area == "cn":
+                for k,v in info.items():
+                    for sn in v:
+                        cn_packet[sn] = k
+            elif area == "de":
+                for k,v in info.items():
+                    for sn in v:
+                        de_packet[sn] = k
+
+
+        #先写入国际段物流信息，因为国际段主键是作为其他表的外键
+        #再写入国内段和德国段物流信息，可以不分先后
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO " + rinterTable + " (`id`, `railway_sn`, `inter_log`, `inter_time`, `inter_status`, `inter_company`) \
+                  VALUES (NULL, %s, '', %s, '-1', %s);"
+            cursor.execute(sql,(railway_sn,timestamp,inter_company))
+
+            #log
+            LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+            logging.basicConfig(filename='mylogis.log',level=logging.DEBUG,format=LOG_FORMAT)
+            logging.info("----------------------------------------------------------------") #分割线
+            logging.info(sql % ("'"+railway_sn+"'","'"+timestamp+"'","'"+inter_company+"'"))
+
+            sql = "SELECT `id` FROM " + rinterTable + " WHERE `railway_sn`=%s"
+            cursor.execute(sql,railway_sn)
+            rinterid = cursor.fetchone()[0]
+            print("inter id : " + str(rinterid))
+
+            for sn,company in cn_packet.items():
+                mainsn = ['']
+                subsns = [[]]
+                companycode =['']
+                notes = [[]]
+                split_token(sn,mainsn,subsns,companycode,notes)
+                sn = mainsn[0] + build_subsn(subsns[0])
+
+                sql = "INSERT INTO " + logiscnTable + " (`id`, `cn_packet_sn`, `railway_id`, `cn_log`, `cn_time`, `cn_status`, `cn_company`) \
+                    VALUES (NULL, %s, %s, '', %s, '-1', %s);"
+                if not company:
+                    company = companycode[0]
+                cursor.execute(sql,(sn,rinterid,timestamp,company))
+                logging.info(sql % ("'"+sn+"'",rinterid,"'"+timestamp+"'","'"+company+"'"))
+
+            for sn,company in de_packet.items():
+                sql = "INSERT INTO " + logisdeTable + " (`id`, `de_packet_sn`, `railway_id`, `de_log`, `de_time`, `de_status`, `de_company`) \
+                    VALUES (NULL, %s, %s, '', %s, '-1', %s);"
+                cursor.execute(sql,(sn,rinterid,timestamp,company))
+                logging.info(sql % ("'"+sn+"'",rinterid,"'"+timestamp+"'","'"+company+"'"))
+
+        connection.commit()
+    finally:
+        connection.close()
+
+    print("Finished.")
+
+
+
 
 
 #从html生成物流表达式
